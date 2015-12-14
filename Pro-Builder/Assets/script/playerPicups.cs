821c4f024e9b03e599db3f111dc3bc17027f54b7
amo110 <abiolaoyelaja@nbp-204-64.nbp.ruw.rutgers.edu>
2015-12-13 22:11:00 -0500

    Finished
    The player(First person mode) must collect all the cubes

---
+++ Pro-Builder/Assets/script/playerPicups.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class playerPicups : MonoBehaviour {
+	
+	public Text countText;
+	public Text winText;
+	private int count;
+
+	void Start ()
+	{
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+			count = count + 1;
+			SetCountText();
+        }
+    }
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString() + " out of 20";
+		if (count >= 20) {
+			winText.text = "YOU WIN!";
+		}
+}
+}
